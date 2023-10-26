275262973bc4acee693c78b38d748f94a6f6922d
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-10-26 16:21:52 +0200

    add comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,9 +30,9 @@
             </ul>
         </div>
 
-        <div action="">
-            <input type="text">
-        </div>
+        <form action="">
+            <input class="" type="text">
+        </form>
     </div>
 </div>
 
@@ -45,8 +45,24 @@
 @endsection
 
 @section('main-content')
-<div class="">
+<div class="container py-5">
+    <div class="row">
 
+        @forelse ($comics as $comic)
+        <div class="col-2 g-3 comic">
+            <div class="wrapper">
+                <img class="img-fluid " src="{{ $comic['thumb'] }}" alt="">
+                <span>{{$comic['title']}}</span>
+            </div>
+        </div>
+        @empty
+        <p class="text-white">No comics very sad</p>
+        @endforelse
+    </div>
+
+    <div class="d-flex justify-content-center align-items-center ">
+        <a class="load_more" href="#">load more</a>
+    </div>
 </div>
 
 @endsection
