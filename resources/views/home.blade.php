65dbf73aee22bcf66ba5fa1c41999cc35c1d08e1
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-10-26 15:49:26 +0200

    add header and jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,59 @@
 @extends('layouts.app')
 
 @section('header-content')
-<header class="p-5 bg-black">
-    <h1>(☞ﾟヮﾟ)☞ we ☜(ﾟヮﾟ☜) </h1>
-</header>
+<div class="top text-white p-1">
+    <div class="container d-flex">
+        <div class=" d-flex ms-auto gap-5">
+            <small class=" text-uppercase">dc power&#8480; visa&#9415;</small>
+            <small class=" text-uppercase">additional dc sites <i class="fa-solid fa-caret-down"></i></small>
+        </div>
+    </div>
+</div>
+
+<div class="bot p-2">
+    <div class="container d-flex justify-content-between align-items-center">
+
+        <div class="logo-wrapper ">
+            <a href="#"><img class="img-fluid" width="75" src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt=""></a>
+        </div>
+
+        <div>
+            <ul class=" list-unstyled d-flex justify-center align-items-center gap-4">
+                <li>menu 1</li>
+                <li>menu 2</li>
+                <li>menu 1</li>
+                <li>menu 2</li>
+                <li>menu 1</li>
+                <li>menu 2</li>
+                <li>menu 1</li>
+                <li>menu 2<i class="fa-solid fa-caret-down"></i></li>
+            </ul>
+        </div>
+
+        <div action="">
+            <input type="text">
+        </div>
+    </div>
+</div>
 
 @endsection
 
+@section('jumbotron-content')
+<div class="text">
+    <h2>current series</h2>
+</div>
+@endsection
+
 @section('main-content')
-<div class="p-5 bg-danger">
-    <h1>(☞ﾟヮﾟ)☞ we ☜(ﾟヮﾟ☜) </h1>
+<div class="">
+
 </div>
 
 @endsection
 
 @section('footer-content')
-<footer class="p-5 bg-secondary">
-    <h1>(☞ﾟヮﾟ)☞ we ☜(ﾟヮﾟ☜) </h1>
-</footer>
+<div class="">
+
+</div>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Styles -->
     @vite('resources/js/app.js')
 
@@ -19,6 +20,10 @@
     <header>
         @yield('header-content')
     </header>
+
+    <div class="jumbotron d-flex align-items-end">
+        @yield('jumbotron-content')
+    </div>
 
     <main>
         @yield('main-content')
